bba3709c152cae603a6c4cc9c4cdca37acaadd99
MuradAles <muradaleskerov99@gmail.com>
2023-06-13 12:50:22 -0400

    small fix in link

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
                 <h2 className='h2_style'>Murad</h2>
                 <p className='TextInfo'>Hello and welcome to my website! I'm a master's student of computer science and my passion lies in creating websites and exploring the fascinating field of machine learning. I believe that the combination of these two fields can create powerful tools that can transform the world we live in. Currently, I'm focusing on expanding my knowledge and skills in these areas. I enjoy staying up-to-date with the latest technologies and trends, and love to challenge myself with new projects. Through my website, I hope to showcase some of my work and share my enthusiasm for these subjects. Feel free to browse through my portfolio and get in touch if you have any questions or ideas for collaboration. Thank you for visiting!</p>
                 <div>
-                    <a className='Links' href="https://www.linkedin.com/in/murad-aleskerov-77a4871b5/"><img src={process.env.PUBLIC_URL + '/images/LinkedInLogo.png'} alt="MyImage" /></a>
+                    <a className='Links' href="https://www.linkedin.com/in/murad-aleskerov/"><img src={process.env.PUBLIC_URL + '/images/LinkedInLogo.png'} alt="MyImage" /></a>
                     <a className='Links' href="https://github.com/MuradAles"><img src={process.env.PUBLIC_URL + '/images/gitLogo.png'} alt="MyImage" /></a>
                 </div>
             </div>
